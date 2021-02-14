bc5da2d3f25a16d046e9fa0e17af9a2444a971a1
Frankie D RSVLT <57404682+Frankie-D-RSVLT@users.noreply.github.com>
2021-02-14 20:11:20 +0100

    Update Copy-ScheduledTask.ps1

--- Copy-ScheduledTask.ps1
+++ Copy-ScheduledTask.ps1
@@ -1,51 +1,62 @@
 ﻿function Copy-ScheduledTask {
-    param (
-        [int]$CopyCounter = 1,
-        [bool]$TargetFolder = $false
-    )
-    $Tasks = @()
-    $ID = 1
-    foreach ($Task in (Get-ScheduledTask)) {
-        $Tasks += [PSCustomObject][ordered]@{
-            'ID'    = $ID++
-            'Name'  = $Task.TaskName
-            'Path'  = $Task.TaskPath
-            'State' = $Task.State
-            'URI'   = $Task.URI
-        }
-    }
+    $script:Tasks | Select-Object -ExcludeProperty Copy, URI | Format-Table -AutoSize
 
-    $Tasks | Select-Object ID, Name, Path | Format-Table -AutoSize
-
+    # ID selection
     $Selected = @()
-    foreach ($Integer in (Read-Host 'Please enter the task IDs to copy. Seperated by a comma, you can use also a ranged selection (eg: 12,30..40,7 | This will select the ask with the ID 12, the tasks with the IDs from 30 to 40 and it will select the task with the ID 7)').Split(',')) {
+    foreach ($SelectedID in (Read-Host '[Q1/2] Please enter the task IDs to copy. Seperated by a comma, you can use also a ranged selection (eg: 12,30..40,7 | This will select the task with the ID 12, the tasks with the IDs from 30 to 40 and it will select the task with the ID 7)').Split(',')) {
 
-        if ($Integer -like "*..*") {
-            $Split = $Integer.Split('..')
+        if ($SelectedID -like "*..*") {
+            $Split = $SelectedID.Split('..')
             $Selected += $Split[0]..$Split[1]
         }
         else {
-            $Selected += $Integer
+            $Selected += $SelectedID
         }
     }
-    
-    foreach ($sTask in ($Tasks | Where-Object ID -in $Selected)) {
-        $Xml = $null
 
-        if ($TargetFolder) {
-            $Path = $TargetFolder
-        }else {
-            $Path = $sTask.Path
+    # path selection
+
+    $UserSelectedPath = Read-Host '[Q2/2] If you want to copy the tasks to another folder please enter the targetfolder, if not just leave the field emnty.'
+    if ($UserSelectedPath) {
+        foreach ($Task in $script:Tasks) {
+            $Task.TargetPath = $UserSelectedPath
         }
+    }
 
-        $Xml = Export-ScheduledTask $sTask.URI
-        ($CopyCounter - 1)..0 | Foreach-Object {
-            try {
-                Register-ScheduledTask -TaskName ($sTask.Name + ' - copy ' + [string](Get-Random)) -TaskPath $Path -Xml $Xml -ErrorAction Stop
-            }
-            catch {
-                Write-Error ('You need elevated privileges to copy the task: ' + $sTask.Name + '. Please re-start the powershell with a admin user or the user account that created/owns the task.')
-            }
+    Use-ScheduledTaskEngine -ID $SelectedTasks -TargetFolder $UserSelectedPath
+}
+
+function Build-ScheduledTaskList {
+    $script:Tasks = @()
+    $ID = 1
+    foreach ($Task in (Get-ScheduledTask)) {
+        $script:Tasks += [PSCustomObject][ordered]@{
+            'ID'         = $ID++
+            'Name'       = $Task.TaskName
+            'Path'       = $Task.TaskPath
+            'State'      = $Task.State
+            'URI'        = $Task.URI
+            'Copy'       = $false
+            'TargetPath' = $Task.TaskPath
+        }
+    }
+}
+
+function Use-ScheduledTaskEngine {
+    param (
+        [string]$ID = '\\\undefined',
+        [string]$TargetFolder = '\\\undefined',
+        [int]$CopyCounter = 1
+    )
+
+    
+
+    ($CopyCounter - 1)..0 | Foreach-Object {
+        try {
+            Register-ScheduledTask -TaskName ($sTask.Name + ' - copy ' + [string](Get-Random)) -TaskPath $Path -Xml (Export-ScheduledTask $sTask.URI) -ErrorAction Stop
+        }
+        catch {
+            Write-Error ('You need elevated privileges to copy the task: ' + $sTask.Name + '. Please re-start the powershell with a admin user or the user account that created/owns the task.')
         }
     }
 }
