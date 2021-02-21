216519986687c05d4504f1f1fb8bf413452c7589
Frankie D RSVLT <57404682+Frankie-D-RSVLT@users.noreply.github.com>
2021-02-21 20:52:20 +0100

    Update Copy-ScheduledTask.ps1

--- Copy-ScheduledTask.ps1
+++ Copy-ScheduledTask.ps1
@@ -3,19 +3,20 @@
         [parameter(ValueFromPipelineByPropertyName, ParameterSetName = 'URI', Position = 0)][string]$URI,
         [parameter(ValueFromPipelineByPropertyName, DontShow)][string]$TaskName,
         [parameter(ValueFromPipelineByPropertyName, DontShow)][string]$TaskPath,
+        [string]$Destination,
         [int]$NumberOfCopies = 1,
         [switch]$DeleteSourceTask = $false
     )
 
-    begin {
-    }
+    begin {}
 
     process {
+        if (!($Destination)){
+            $Destination = $TaskPath
+        }
         [int]$CopyNumber = 1
-
         foreach ($TaskToCopy in $URI) {
             do {
-
                 if ($CopyNumber -eq 1) {
                     $CopyTaskName = $TaskName + ' - copy'
                 }
@@ -23,21 +24,29 @@
                     $CopyTaskName = $TaskName + ' - copy ' + $CopyNumber
                 }
             
-                if ($CopyTaskName -eq (Get-ScheduledTask $CopyTaskName -ErrorAction Ignore).TaskName) {
-                    $CopyTaskName = $TaskName + ' ' + (New-Guid).Guid
+                if ((Get-ScheduledTask -TaskName $CopyTaskName -TaskPath $Destination -ErrorAction Ignore).TaskName) {
+                    $CopyTaskName = $TaskName + ' - copy ' + (New-Guid).Guid
                 }
             
-            
+                try {
+                    Register-ScheduledTask -TaskName $CopyTaskName -TaskPath $Destination -Xml (Export-ScheduledTask $URI) -ErrorAction Stop
+                }
+                catch {
+                    Write-Error ('You need elevated privileges to copy the task: ' + $TaskName + '. Please re-start the powershell with a admin user or the user account that created/owns the task.')
+                }       
+            }until ($CopyNumber++ -eq $NumberOfCopies)
+        }
+    }
+
+    end {
+        if ($DeleteSourceTask) {
             try {
-                Register-ScheduledTask -TaskName $CopyTaskName -TaskPath $TaskPath -Xml (Export-ScheduledTask $URI) -ErrorAction Stop
+                Unregister-ScheduledTask -TaskPath $TaskPath -TaskName $TaskName -Confirm:$false -ErrorAction Stop
             }
             catch {
-                Write-Error ('You need elevated privileges to copy the task: ' + $TaskName + '. Please re-start the powershell with a admin user or the user account that created/owns the task.')
-            }       
-        }until ($CopyNumber++ -eq $NumberOfCopies)
+                Write-Error ('You need elevated privileges to delete the task: ' + $TaskName + '. Please re-start the powershell as a admin user.')
+            }
+            Write-Warning ('Task: ' + $URI + ' deleted.')
+        }
     }
-
-    }
-
-    end {}
 }
